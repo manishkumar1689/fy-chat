c7af9a61cd61eeac86d392df2ed8890d923320f5
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2022-02-04 12:47:12 +0000

    corrected individual unread count

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -231,7 +231,7 @@ export class ChatService {
       ids.push(fromId);
       if (ui instanceof Object && notEmptyString(ui.nickName)) {
         const last = await this.fetchLastMicroMessage(fromId, userId);
-        const numUnread = await this.countUnread(fromId, userId);
+        const numUnread = await this.countUnread(userId, fromId);
         items.push({ ...ui, last, numUnread, hasReplied: true } as BasicInfo);
       }
     }
