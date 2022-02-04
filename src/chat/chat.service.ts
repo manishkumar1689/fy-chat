0c82db2d8b83c4cb0c8480fbfff90eaf9b8b4075
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2022-02-04 13:46:45 +0000

    corrected individual unread count

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -210,7 +210,7 @@ export class ChatService {
     if (validFromId && validToId) {
       filter.set('to', toId);
       filter.set('from', fromId);
-      filter.set('read', { read: { $ne: true } });
+      filter.set('read', { $ne: true });
       filter.set('time', { $gte: fromTs });
       const countNum = await this.chatModel
         .count(Object.fromEntries(filter.entries()))
